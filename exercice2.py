87eb7000568ce05724933e1fe218596cf3696b01
Miasorrow <manon.arnaud12@gmail.com>
2025-11-28 13:10:08 +0100

    Enlever commentaire

--- exercice2.py
+++ exercice2.py
@@ -9,21 +9,16 @@ grid[887:959+1,9:629+1] +=1
 grid[454:844+1,398:448+1] += 1
 grid[539:559+1,243:965+1] -= 1
 grid[grid < 0] = 0
-#grid = np.maximum(grid, 0)
 grid[370:676+1,819:868+1] -= 1
 grid[grid < 0] = 0
-#grid = np.maximum(grid, 0)
 grid[145:370+1,40:997+1] -= 1
 grid[grid < 0] = 0
-#grid = np.maximum(grid, 0)
 grid[301:808+1,3:453+1] -= 1
 grid[grid < 0] = 0
-#grid = np.maximum(grid, 0)
 grid[351:951+1,678:908+1] +=1
 # Changer le true en false ou inversement
 grid[720:897+1,196:994+1] +=2
 grid[831:904+1,394:860+1] +=2
-
 total = np.sum(grid)
 print(total)
 
